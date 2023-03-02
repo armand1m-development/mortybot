1db682bd17cef81cabee50ae40c6636c7052e388
Armando Magalhaes <amagalhaes@netflix.com>
2023-03-03 00:48:32 +0100

    fix: markdown formatter and member mention library

--- src/skills/goodbye/commands/cmdLeavingRank.ts
+++ src/skills/goodbye/commands/cmdLeavingRank.ts
@@ -1,21 +1,30 @@
 import { CommandMiddleware } from "grammy/composer.ts";
 import { BotContext } from "/src/context/mod.ts";
+import { createMemberMention } from "/src/utilities/createMemberMention.ts";
 
 export const cmdLeavingRank: CommandMiddleware<BotContext> = async (ctx) => {
   const goodbyeCounter = [...ctx.session.goodbyeCounter.entries()]
     .sort(([_keyA, valueA], [_keyB, valueB]) => {
-      return valueA.count - valueB.count;
+      return valueB.count - valueA.count;
     });
 
-  const rank = goodbyeCounter.map(([userId, metadata], index) => {
-    const rank = index + 1;
+  const rank = await Promise.all(
+    goodbyeCounter.map(async ([userId, metadata], index) => {
+      const rank = index + 1;
 
-    if (rank === 1) {
-      return `🏆 ${rank}. ${userId} - left the group ${metadata.count} times`;
-    }
+      const { user } = await ctx.getChatMember(userId);
 
-    return `${rank}. ${userId} - left the group ${metadata.count} times`;
-  });
+      if (rank === 1) {
+        return `${rank}. ${
+          createMemberMention(user, false)
+        } - left the group ${metadata.count} times (the winner! 🏆)`;
+      }
+
+      return `${rank}. ${
+        createMemberMention(user, false)
+      } - left the group ${metadata.count} times`;
+    }),
+  );
 
   if (rank.length === 0) {
     return ctx.reply("Nobody ever left this group (at least not that I know).");
---
+++ src/utilities/createMemberMention.ts
@@ -0,0 +1,15 @@
+import { User } from "grammy/types.ts";
+import { markdown } from "/src/utilities/formatMarkdown.ts";
+
+export const createMemberMention = (
+  user: User,
+  silenced = false,
+) => {
+  const username = user.username ? `@${user.username}` : user.first_name;
+
+  if (silenced) {
+    return markdown.monospace(username);
+  }
+
+  return markdown.userMention(username, user.id);
+};
