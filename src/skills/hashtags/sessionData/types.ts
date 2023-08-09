1f397cc85aac734cc78dbf626e98cb0e04a1f2d5
Rafael Martins <rafaelplmartins@hotmail.com>
2023-08-09 17:28:42 -0300

    Tag Channel feature (#6)
    * feat: create join command
    
    * chore: fix lockfile
    
    * refactor: rename to hashtags, set initial structure, listener pending
    
    * feat: hashtag feature
    
    ---------
    
    Co-authored-by: Armando Magalhaes <amagalhaes@netflix.com>

---
+++ src/skills/hashtags/sessionData/types.ts
@@ -0,0 +1,10 @@
+import { User } from "grammy/types.ts";
+
+export interface HashtagChannel {
+  hashtag: string;
+  participants: User["id"][];
+}
+
+export interface HashtagChannelSessionData {
+  hashtagChannels: Map<string, HashtagChannel>;
+}
--- src/skills/skills.ts
+++ src/skills/skills.ts
@@ -7,6 +7,7 @@ export const skills = [
   "espiritosanto",
   "goodbye",
   "weather",
+  "hashtags",
   "currency",
 ] as const;
 
