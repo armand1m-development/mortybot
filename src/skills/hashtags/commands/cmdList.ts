0e8e1c2153b4f6d4260e2901f0606da8d0a8bf75
Armando Magalhães <armando.mag95@gmail.com>
2023-10-24 16:24:08 -0300

    feat: listing improvements (#15)
    * feat: improve listing with chunking. add /hashtags
    
    * docs: update readme

--- src/skills/hashtags/commands/cmdList.ts
+++ src/skills/hashtags/commands/cmdList.ts
@@ -2,20 +2,20 @@ import { CommandMiddleware } from "grammy/composer.ts";
 import { getChunks } from "/src/utilities/array/getChunks.ts";
 import { BotContext } from "/src/context/mod.ts";
 
-export const cmdListFilters: CommandMiddleware<BotContext> = async (ctx) => {
-  const filters = Object.fromEntries(ctx.session.filters);
+export const cmdListHashtags: CommandMiddleware<BotContext> = async (ctx) => {
+  const filters = Object.fromEntries(ctx.session.hashtagChannels);
   const entries = Object.entries(filters);
 
   if (entries.length === 0) {
-    await ctx.reply("There are no filters defined for this group currently.");
+    await ctx.reply("There are no hashtags defined for this group currently.");
     return;
   }
 
   const chunkedEntries = getChunks(entries, 100);
 
   for (const entrySet of chunkedEntries) {
-    const lines = entrySet.map(([filterTrigger, filter]) => {
-      return `- ${filterTrigger} (${filter.active ? "active" : "stopped"})`;
+    const lines = entrySet.map(([hashtag, hashtagProps]) => {
+      return `- ${hashtag} (${hashtagProps.participants.length} registered)`;
     });
 
     await ctx.reply(lines.join("\n"));
