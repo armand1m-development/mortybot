167c32103c0ee3ce58203057be0344292e0c1d8b
Armando Magalhaes <amagalhaes@netflix.com>
2024-03-05 23:52:54 +0100

    fix: audio

--- src/skills/filters/utilities/mapAudioToInlineQueryResult.ts
+++ src/skills/filters/utilities/mapAudioToInlineQueryResult.ts
@@ -5,7 +5,12 @@ export const mapAudioToInlineQueryResult = (
   audio: Audio,
 ): InlineQueryResult => ({
   type: "audio",
-  audio_url: `https://mortybot.fly.dev/audio/${audio.id}`,
+  audio_url: `https://mortybot.fly.dev/audio/${audio.id}.${audio.extension}`,
   id: audio.id,
   title: audio.name,
+  input_message_content: {
+    message_text:
+      `🎵 [${audio.name}](https://mortybot.fly.dev/audio/${audio.id}.${audio.extension})`,
+    parse_mode: "Markdown",
+  },
 });
