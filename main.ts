2466a06fb87bad3d2b6c069a963e0481129f7df0
Shayron Aguiar <shayron.aguiar@gmail.com>
2024-06-02 11:06:32 -0300

    wip: wip on feature/templates-api

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
 import { createBot } from "/src/bot.ts";
 import { run } from "grammy_runner/mod.ts";
 import { serveDir } from "std/http/file_server.ts";
+import { Application, Router, serve } from "oak";
 import * as log from "std/log/mod.ts";
 import * as dotenv from "std/dotenv/mod.ts";
 import * as path from "std/path/mod.ts";
+import { bold, yellow } from "std/fmt/colors.ts";
+import { reviver } from "/src/utilities/jsonParsing.ts";
+import { replacer } from "./src/utilities/jsonParsing.ts";
+
+async function readJsonFile(filePath: string) {
+  const jsonContent = await Deno.readTextFile(filePath);
+  const data = JSON.parse(jsonContent, reviver);
+  return data;
+}
 
 log.setup({
   handlers: {
@@ -21,6 +31,10 @@ await dotenv.load({
   export: true,
 });
 
+const INLINE_QUERY_SOURCE_CHAT_ID = Deno.env.get(
+  "INLINE_QUERY_SOURCE_CHAT_ID",
+)!;
+
 const { bot, configuration } = await createBot({
   dataPath: Deno.env.get("DATA_PATH")!,
   botToken: Deno.env.get("BOT_TOKEN")!,
@@ -28,16 +42,31 @@ const { bot, configuration } = await createBot({
   openWeatherMapApiToken: Deno.env.get("OPENWEATHERMAP_API_TOKEN")!,
   googleMapsApiToken: Deno.env.get("GOOGLEMAPS_API_TOKEN")!,
   n2yoApiToken: Deno.env.get("N2YO_API_TOKEN")!,
-  inlineQuerySourceChatId: Deno.env.get("INLINE_QUERY_SOURCE_CHAT_ID")!,
+  inlineQuerySourceChatId: INLINE_QUERY_SOURCE_CHAT_ID,
 });
 
 const botInstance = run(bot);
 
 const serverAbortController = new AbortController();
-const staticServerInstance = Deno.serve({
-  port: 3000,
-  signal: serverAbortController.signal,
-}, (req: Request) => {
+
+const router = new Router();
+
+router.get("/templates", async (ctx) => {
+  const dataBase = await readJsonFile(
+    path.join(
+      configuration.dataPath,
+      `/sessions/66/${INLINE_QUERY_SOURCE_CHAT_ID}.json`,
+    ),
+  );
+  console.log(dataBase);
+  ctx.response.body = JSON.stringify(dataBase, replacer);
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(serve((req: Request) => {
   const pathname = new URL(req.url).pathname;
 
   if (pathname.startsWith("/audio")) {
@@ -51,12 +80,20 @@ const staticServerInstance = Deno.serve({
   return new Response("404: Not Found", {
     status: 404,
   });
+}));
+
+app.addEventListener("listen", ({ hostname, port, serverType }) => {
+  console.log(
+    bold("Start listening on ") + yellow(`${hostname}:${port}`),
+  );
+  console.log(bold("  using HTTP server: " + yellow(serverType)));
 });
+
+app.listen({ port: 8000, signal: serverAbortController.signal });
 
 const stopServers = () => {
   botInstance.isRunning() && botInstance.stop();
   serverAbortController.abort("shutdown");
-  staticServerInstance.finished.then(() => console.log("server has shutdown"));
 };
 
 Deno.addSignalListener("SIGINT", stopServers);
